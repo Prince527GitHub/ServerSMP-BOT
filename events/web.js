b0b060b669392624c114c46cf9a5e1756e83f75e
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 12:30:41 -0700

    Updated the web stuff

--- events/web.js
+++ events/web.js
@@ -1,13 +1,10 @@
 const client = require('../index');
 const express = require("express");
 const path = require('path');
-const serveIndex = require('serve-index');
 const mongoose = require('mongoose');
 const model = require('../models/economy');
 const { getCommands } = require('../utils/index');
 const app = express();
-//const socketStats = require("socketstats");
-//const server = new socketStats(app, client);
 const _PORT = process.env.PORT || 8080;
 
 client.on('ready', async () => {
@@ -16,12 +13,12 @@ client.on('ready', async () => {
     users: client.users.cache.size,
     channels: client.channels.cache.size
   }
-  app.set("view engine", "ejs");
-  app.use("/", express.static('website'));
-  app.use("/", serveIndex('website'));
+  app.get("/", (req, res) => {
+    res.status(200).render('index.pug')
+  })
   app.get("/commands", (req, res) => {
     const commands = getCommands();
-    res.status(200).render('commands', { commands })
+    res.status(200).render('commands.ejs', { commands })
   })
   app.get("/economy", (req, res) => {
     const user = req.query.user;
@@ -38,6 +35,5 @@ client.on('ready', async () => {
     res.status(200).send(clientDetails)
   })
   app.listen(_PORT)
-  //server.listen(8080)
   console.log(`Listening to ports: ${_PORT}`);
 })
