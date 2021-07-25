31c1918352a8015ff363ab3b87472b57e52783d1
Prince527GitHub <75485262+Prince527GitHub@users.noreply.github.com>
2021-07-25 13:28:00 -0300

    Delete models directory

--- models/birthday.js
+++
@@ -1,6 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-module.exports = model('birthday', new Schema({
-    User: String,
-    Birthday: String,
-}));
--- models/economy.js
+++
@@ -1,6 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-module.exports = model('economy', new Schema({
-    id: String,
-    coins: Number
-}));
