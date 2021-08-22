6189634101ab20bd10d0ff271121546d3a7a813f
Prince527GitHub <75485262+Prince527GitHub@users.noreply.github.com>
2021-08-21 23:40:02 -0300

    Delete models directory

--- models/global.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-module.exports = model('channels', new Schema({
-    Guild: String,
-    Channel: String,
-    Author: String,
-    Activated: Boolean,
-}));
