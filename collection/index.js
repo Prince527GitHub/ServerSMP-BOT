f21855fa5eed3a6e53aa8cf417088950f2280ad2
Prince527GitHub <75485262+Prince527GitHub@users.noreply.github.com>
2021-08-21 23:39:39 -0300

    Delete collection directory

--- collection/index.js
+++
@@ -1,9 +0,0 @@
-const { Collection } = require('discord.js');
-
-const antijoin = new Collection();
-const afk = new Collection();
-const snipe = new Collection();
-const blacklistedwords = new Collection();
-const music = new Collection();
-
-module.exports = { antijoin, afk, snipe, blacklistedwords, music }
