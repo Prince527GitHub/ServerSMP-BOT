9acbeb0cfebc2b82aed1e71f66bac57a6a826677
Prince527GitHub <75485262+Prince527GitHub@users.noreply.github.com>
2022-02-17 20:40:50 -0400

    SW update

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,4 +1,4 @@
-importScripts('./workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 workbox.routing.registerRoute(
     ({request}) => require.destination === 'image',
